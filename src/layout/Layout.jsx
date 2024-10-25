f524fcd4705daf519cdb82c4a86bb821417e2e9b
aryan <aryanpidiha.dev@gmail.com>
2024-10-25 06:46:47 +0530

    added right sidebar

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/sidebar/Sidebar";
 import Header from "../components/header/Header";
+import RightSidebar from "../components/right-sidebar/RightSidebar";
 
 const Layout = () => {
   return (
@@ -12,9 +13,7 @@ const Layout = () => {
           <Outlet />
         </div>
       </div>
-      <div className="flex flex-col h-screen w-[280px] px-5 py-6 gap-8 border-l border-[#1C1C1C1A] dark:border-[#FFFFFF1A] overflow-y-auto scroll-hidden">
-        Notifications
-      </div>
+      <RightSidebar />
     </div>
   );
 };
